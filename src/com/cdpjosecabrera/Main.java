74f71bc3189716d9a197b96b15ffca9937d86e19
agvarelapru <agvarelapru@gmail.com>
2017-05-09 12:55:53 +0200

    suma

--- src/com/cdpjosecabrera/Main.java
+++ src/com/cdpjosecabrera/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
+		
+		Operaciones ope=new Operaciones();
+		
+		int suma=ope.suma(5, 6);
+		System.out.println("El resultado es ..." +suma);
+		
 
 	}
 
---
+++ src/com/cdpjosecabrera/Operaciones.java
@@ -0,0 +1,15 @@
+package com.cdpjosecabrera;
+
+public class Operaciones {
+
+	
+	public int suma(int num1, int num2){
+		
+		int suma=num1+num2;
+		return suma;
+
+	}
+	
+	
+	
+}
