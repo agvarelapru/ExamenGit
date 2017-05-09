52875ae919ea89115aad20107fdda8c86b05dfeb
agvarelapru <agvarelapru@gmail.com>
2017-05-09 12:58:57 +0200

    Revert "resta"
    This reverts commit a8278be9bb0bb869d0cd75b455a0a044b7b24a24.

--- src/com/cdpjosecabrera/Main.java
+++ src/com/cdpjosecabrera/Main.java
@@ -10,10 +10,6 @@ public class Main {
 		int suma=ope.suma(5, 6);
 		System.out.println("El resultado es ..." +suma);
 		
-		int resta=ope.resta(5, 6);
-		System.out.println("El resultado es ..." +resta);
-		
-		
 
 	}
 
--- src/com/cdpjosecabrera/Operaciones.java
+++ src/com/cdpjosecabrera/Operaciones.java
@@ -10,11 +10,6 @@ public class Operaciones {
 
 	}
 	
-public int resta(int num1, int num2){
-		
-		int resta=num1-num2;
-		return resta;
-
-	}
+	
 	
 }
